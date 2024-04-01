39dbb8368fecccac9d2f2c800608cd4ab31a9621
deepanshu1069 <139270250+deepanshu1069@users.noreply.github.com>
2024-04-01 22:29:31 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+const token = "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";
+const inputTxt = document.getElementById("input");
+const image = document.getElementById("image");
+const button = document.getElementById("btn");
+
+
+async function query(data) {
+    image.src= "load-loading.gif"
+	const response = await fetch(
+		"https://api-inference.huggingface.co/models/Melonie/text_to_image_finetuned",
+		{
+			headers: { Authorization: `Bearer ${token} ` },
+			method: "POST",
+            body: JSON.stringify({ inputs: inputTxt.value }),
+		}
+	);
+	const result = await response.blob();
+	return result;
+}
+button.addEventListener("click", async function () {
+    query().then((response) => {
+      const objectURL = URL.createObjectURL(response);
+      image.src = objectURL;
+    });
+  });
